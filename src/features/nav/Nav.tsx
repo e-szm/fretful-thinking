a2c8a1635942908672679d25aacd79eda3f549f1
Eddie Szmigielski <157999244+e-szm@users.noreply.github.com>
2024-12-17 09:35:55 -0600

    Add logo on nav and favicon

--- src/features/nav/Nav.tsx
+++ src/features/nav/Nav.tsx
@@ -1,7 +1,15 @@
 import styles from "./Nav.module.css";
 
 const Nav: React.FC = () => {
-  return <div className={styles.nav}></div>;
+  return (
+    <div className={styles.nav}>
+      <img
+        className={styles.logo}
+        src="/logo.png"
+        alt="Fretful Thinking logo"
+      />
+    </div>
+  );
 };
 
 export default Nav;
