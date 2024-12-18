214f742ed22fbeb727844aad70b9e4a321fe5ec4
Eddie Szmigielski <157999244+e-szm@users.noreply.github.com>
2024-12-18 11:12:47 -0600

    Add dynamic frets that adjust width based on fret number. Minor enhancements to fretboard logic, including removing inlay from fret 1.

--- src/features/guitar/fretboard/fret/Fret.tsx
+++ src/features/guitar/fretboard/fret/Fret.tsx
@@ -6,22 +6,26 @@ import String from "../string/String";
 import Note from "../../../../shared/classes/Note";
 
 interface FretProps {
-  num: number;
+  fretNum: number;
   fret: Array<Note | null>;
   noteFilter: string | undefined;
 }
 
-const Fret: React.FC<FretProps> = ({ num, fret, noteFilter }) => {
+const Fret: React.FC<FretProps> = ({ fretNum, fret, noteFilter }) => {
   const [{ view }] = useGuitarQuery();
 
   // Guitar inlays typically appear on 1, 3, 5, 7, 9
   // On fret 12, there is typically a double inlay
-  const doubleInlay = num === 12;
-  const showInlay = (num % 2 !== 0 && num !== 11 && num !== 13) || doubleInlay;
+  const doubleInlay = fretNum === 12;
+  const showInlay =
+    (fretNum % 2 !== 0 && fretNum !== 1 && fretNum !== 11 && fretNum !== 13) ||
+    doubleInlay;
+
+  const width = 115 - (fretNum - 1) * 6 + "%";
 
   return (
     <>
-      <div className={styles.fret}>
+      <div className={styles.fret} style={{ width }}>
         {showInlay && (
           <div className={styles.inlayContainer}>
             <div className={styles.inlay}></div>
